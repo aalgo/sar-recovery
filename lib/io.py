3b7100e0ebcfcfbc5cf021bea2104b93a7b41116
Alberto Alonso-Gonzalez <alberto.alons@gmail.com>
2024-03-01 12:11:23 +0100

    Added a function load_bcn_scatter_vector_pauli() in lib/io.py.
    This function loads the Pauli scattering vector from the SLCs.

--- lib/io.py
+++ lib/io.py
@@ -30,3 +30,23 @@ def load_bcn_scatter_matrix(base_path, date):
     sm = sm.reshape(*sm.shape[:2], 2, 2)
 
     return sm
+
+def load_bcn_scatter_vector_pauli(base_path, date):
+    """
+    loads the four hh hv vh vv files for a 'date' and puts them together
+    into a Pauli scattering vector w of shape 4402x1602x3 so that
+    [:,:,0] is (Shh + Svv) / sqrt(2)  # Pauli1
+    [:,:,1] is (Shh - Svv) / sqrt(2)  # Pauli2
+    [:,:,2] is (Shv + Svh) / sqrt(2)  # Pauli3
+    """
+    shh = load_bcn_slc(base_path, date, 'HH')
+    shv = load_bcn_slc(base_path, date, 'HV')
+    svh = load_bcn_slc(base_path, date, 'VH')
+    svv = load_bcn_slc(base_path, date, 'VV')
+    w = np.asarray([(shh + svv) / np.sqrt(2),
+                    (shh - svv) / np.sqrt(2),
+                    (shv + svh) / np.sqrt(2),
+                    ])
+    w = np.transpose(w, [1,2,0])
+
+    return w
